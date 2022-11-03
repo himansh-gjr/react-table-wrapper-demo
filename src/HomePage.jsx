c4861957340c5fb117cfd9f6d5abe2715f2efef4
himansh-gjr <himanshugjr1810@gmail.com>
2022-11-03 17:02:49 +0530

    open tables in new tab

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,10 +5,10 @@ export const HomePage = () => {
 	return (
 		<div className="home-page-container">
 			<div className="table-links">
-				<Link to={'basic-table'} ><h3>Basic Table</h3></Link>
-				<Link to={'row-select-table'} ><h3>RowSelect Table</h3></Link>
-				<Link to={'sorting-table'} ><h3>Sorting Table</h3></Link>
-				<Link to={'demo-table'} ><h3>Complete table Demo</h3></Link>
+				<Link target={"_blank"} to={'basic-table'} ><h3>Basic Table</h3></Link>
+				<Link target={"_blank"} to={'row-select-table'} ><h3>RowSelect Table</h3></Link>
+				<Link target={"_blank"} to={'sorting-table'} ><h3>Sorting Table</h3></Link>
+				<Link target={"_blank"} to={'demo-table'} ><h3>Complete table Demo</h3></Link>
 			</div>
 		</div>
 	)
